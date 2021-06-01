bac69e5ae52f00f1bb933b517b4dc330575fd8a4
sowmya.matsa <sowmya.matsa@gmail.com>
2021-06-01 17:52:25 +0530

    recentrecharges has been added

--- recharging/admin.py
+++ recharging/admin.py
@@ -1,19 +1,20 @@
 from django.contrib import admin
-from .models import CustomUser, Plan, Recharge
+from .models import CustomUser, Plan, Recharge,RecentRecharge
 from django.contrib.auth.admin import UserAdmin
 
 
 # Register your models here.
-
-
 class PlanAdmin(admin.ModelAdmin):
-    list_display = ['id',"operator", 'type', 'amount', 'validity']
+    list_display = ['id', "operator", 'type', 'amount', 'validity']
 
 
 class RechargeAdmin(admin.ModelAdmin):
     list_display = ['id', 'recharge_number', 'plan', 'user']
 
+class RecentRechargeAdmin(admin.ModelAdmin):
+    list_display = ['id','recharged_number','date']
 
 admin.site.register(CustomUser)
 admin.site.register(Plan, PlanAdmin)
 admin.site.register(Recharge, RechargeAdmin)
+admin.site.register(RecentRecharge,RecentRechargeAdmin)
--- recharging/models.py
+++ recharging/models.py
@@ -12,13 +12,13 @@ class CustomUser(AbstractUser):
 
 
 class Plan(models.Model):
-    operator = models.CharField(default="",max_length=255)
+    operator = models.CharField(default="", max_length=255)
     type = models.CharField(max_length=255)
     amount = models.IntegerField()
     validity = models.CharField(max_length=255)
 
     def __str__(self):
-        return str(self.operator)+","+str(self.type)
+        return str(self.operator) + "," + str(self.type)
 
 
 class Recharge(models.Model):
@@ -28,3 +28,11 @@ class Recharge(models.Model):
 
     class Meta:
         verbose_name_plural = "recharge"
+
+    def __str__(self):
+        return str(self.recharge_number) + "," + str(self.plan)
+
+
+class RecentRecharge(models.Model):
+    recharged_number = models.ForeignKey(Recharge, on_delete=models.SET_NULL, default=None, blank=True, null=True)
+    date = models.DateField()
